d5d3d91874fe10fc3e2a5e5364e992fee73ae56f
Samantha Ostrowski <isphinxs@hotmail.com>
2021-02-26 19:40:14 -0500

    commit tree fc03ffc1bbedb026dbe62c39f6973e2785cac566

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+require "./config/environment.rb"
+require "sinatra/base"
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -5,5 +8,44 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get "/recipes" do
+    @recipes = Recipe.all
+    erb :index
+  end
 
+  get "/recipes/new" do
+    erb :new
+  end
+
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch "/recipes/:id" do
+    recipe = Recipe.find(params[:id])
+    recipe.name = params[:name]
+    recipe.ingredients = params[:ingredients]
+    recipe.cook_time = params[:cook_time]
+    recipe.save
+    redirect "/recipes/#{params[:id]}"
+  end
+
+  post "/recipes" do
+    recipe = Recipe.new(params)
+    recipe.save
+    id = recipe.id
+    redirect "/recipes/#{id}"
+  end
+
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  delete "/recipes/:id" do
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect "/recipes"
+  end
 end
